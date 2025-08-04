9b7ad3797fadc5b9e6fbd467e2ad77f1dda3d25c
Enrique Cametti <quiquecametti97@gmail.com>
2025-08-04 19:55:22 -0300

    Initial commit.
    Terraform module bootstrap a Talos cluster from a list of specified
    nodes.

---
+++ main.tf
@@ -0,0 +1,64 @@
+resource "talos_machine_secrets" "this" {}
+
+data "talos_client_configuration" "this" {
+  cluster_name         = var.cluster_name
+  client_configuration = talos_machine_secrets.this.client_configuration
+  nodes                = [for k, v in var.talos_nodes : v.ip_address]
+  endpoints            = [for k, v in var.talos_nodes : v.ip_address if v.machine_type == "controlplane"]
+}
+
+data "talos_machine_configuration" "this" {
+  for_each = var.talos_nodes
+
+  talos_version    = var.talos_version
+  cluster_name     = var.cluster_name
+  cluster_endpoint = "https://${local.cluster_endpoint}:6443"
+  machine_type     = each.value.machine_type
+  machine_secrets  = talos_machine_secrets.this.machine_secrets
+}
+
+resource "talos_machine_configuration_apply" "this" {
+  for_each = var.talos_nodes
+
+  client_configuration        = talos_machine_secrets.this.client_configuration
+  machine_configuration_input = data.talos_machine_configuration.this[each.key].machine_configuration
+  node                        = each.value.ip_address
+}
+
+resource "talos_machine_bootstrap" "this" {
+  depends_on = [talos_machine_configuration_apply.this]
+  # Bootstrap with the first control plane node.
+  # VIP not yet available at this stage, so can't use var.cluster.vip
+  # ref - https://www.talos.dev/v1.9/talos-guides/network/vip/#caveats
+  node                 = local.first_control_plane_node_ip
+  client_configuration = talos_machine_secrets.this.client_configuration
+}
+
+data "talos_cluster_health" "this" {
+  depends_on = [
+    talos_machine_configuration_apply.this,
+    talos_machine_bootstrap.this
+  ]
+  skip_kubernetes_checks = false
+  client_configuration   = data.talos_client_configuration.this.client_configuration
+  control_plane_nodes    = [for k, v in var.talos_nodes : v.ip_address if v.machine_type == "controlplane"]
+  worker_nodes           = [for k, v in var.talos_nodes : v.ip_address if v.machine_type == "worker"]
+  endpoints              = data.talos_client_configuration.this.endpoints
+  timeouts = {
+    read = "10m"
+  }
+}
+
+resource "talos_cluster_kubeconfig" "this" {
+  depends_on = [
+    talos_machine_bootstrap.this,
+    data.talos_cluster_health.this
+  ]
+  # The kubeconfig endpoint will be populated from the talos_machine_configuration cluster_endpoint
+  node                 = local.first_control_plane_node_ip
+  client_configuration = talos_machine_secrets.this.client_configuration
+  timeouts = {
+    read = "1m"
+  }
+}
+
---
+++ variables.tf
@@ -0,0 +1,29 @@
+variable "cluster_name" {
+  description = "Talos cluster name"
+  type        = string
+  default     = "talos"
+  sensitive   = false
+}
+
+variable "cluster_vip" {
+  description = "Talos cluster control plane VIP"
+  type        = string
+  nullable    = true
+  sensitive   = false
+  default     = null
+}
+
+variable "talos_nodes" {
+  type = map(object({
+    ip_address   = string
+    ip_subnet    = number
+    machine_type = string
+  }))
+}
+
+variable "talos_version" {
+  description = "Talos node version"
+  type        = string
+  sensitive   = false
+  default     = "v1.10.6"
+}
